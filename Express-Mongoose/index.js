032169fb775e3a9946e2692bbb5bc2f8d2bcb9f1
Gabriel V. Silva <karciel@outlook.com>
2025-04-22 21:41:32 -0300

    feat(server): connect to MongoDB and add basic Express setup

---
+++ Express-Mongoose/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const port = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+async function main() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.aird589.mongodb.net/farmStand`
+    );
+    console.log("Connected to MongoDB server successfully.");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+}
+
+app.get('/dog', (req, res) => {
+  res.send('WOOF!!!')
+})
+
+app.listen(port, () => {
+  console.log(`APP IS LISTENING ON PORT ${port}`);
+});
+
